10b1ad2f83d3041463d90ac7ae32ee85efccd478
Eshal109 <eshal.kashif@stu.ocsb.ca>
2024-11-04 15:33:16 +0000

    Assignment 3

--- handler.php
+++ handler.php
@@ -1,23 +1,38 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
-  </head>
 
-  <body>
-    
-    <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
-    }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
-    ?>
-    
-  </body>
-  
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Welcome Page</title>
+</head>
+
+<body>
+
+  <?php
+  $baseprice = [
+    'large' => 6.00,
+    'extra_large' => 10.00
+  ];
+  $topping_cost = [1, 1.75, 2.50, 3.35];
+  $hst = 0.13;
+  $topping_costs = $toppings_cost[$toppings_cost - 1];
+  $subtotal = $baseprice + $topping_costs;
+  $tax = $subtotal * $hst;
+  $final_cost = $subtotal + $tax;
+
+  if (isset($_POST['baseprice'])) {
+    $myVariable = $_POST['baseprice'];
+  } else;
+  $baseprice = 0;
+  echo "<h1> topping cost is,.$topping_costs;</h1>\n";
+  echo "<h1>Subtotal is,.$subtotal;</h1>\n";
+  echo "<h1>Tax is,.$tax;</h1>\n";
+  echo "<h1>Final cost is,.$final_cost;</h1>\n";
+  echo "<p>My Variable is = " . $baseprice . "</p>\n";
+  ?>
+
+</body>
+
 </html>
