cbcc66fb71a3eb83fbdb7d0caebee624db227885
Eshal109 <eshal.kashif@stu.ocsb.ca>
2024-11-04 15:48:33 +0000

    Assignment 3

--- handler.php
+++ handler.php
@@ -11,20 +11,24 @@
 <body>
 
   <?php
+  $size = "";
+  if (isset($_POST['size'])) {
+    $myVariable = $_POST['size'];
+  } else;
+  $number_toppings = -1;
+  if (isset($_POST['number_toppings'])) {
+    $myVariable = $_POST['number_toppings'];
+  } else;
   $baseprice = [
     'large' => 6.00,
     'extra_large' => 10.00
   ];
   $topping_cost = [1, 1.75, 2.50, 3.35];
   $hst = 0.13;
-  $topping_costs = $toppings_cost[$toppings_cost - 1];
+  $topping_costs = $toppings_cost[$number_toppings - 1];
   $subtotal = $baseprice + $topping_costs;
   $tax = $subtotal * $hst;
   $final_cost = $subtotal + $tax;
-
-  if (isset($_POST['baseprice'])) {
-    $myVariable = $_POST['baseprice'];
-  } else;
   $baseprice = 0;
   echo "<h1> topping cost is,.$topping_costs;</h1>\n";
   echo "<h1>Subtotal is,.$subtotal;</h1>\n";
