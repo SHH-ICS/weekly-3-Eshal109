95432775fd0ac24c7bdf1aebd0d93103fe9e2867
Eshal109 <eshal.kashif@stu.ocsb.ca>
2024-11-05 15:17:31 +0000

    Assignment 3

--- handler.php
+++ handler.php
@@ -26,15 +26,15 @@
   $topping_cost = [1, 1.75, 2.50, 3.35];
   $hst = 0.13;
   $topping_costs = $toppings_cost[$number_toppings - 1];
-  $subtotal = $baseprice + $topping_costs;
+  $subtotal = $baseprice[$size] + $topping_costs;
   $tax = $subtotal * $hst;
   $final_cost = $subtotal + $tax;
   $baseprice = 0;
-  echo "<h1> topping cost is,.$topping_costs;</h1>\n";
-  echo "<h1>Subtotal is,.$subtotal;</h1>\n";
-  echo "<h1>Tax is,.$tax;</h1>\n";
-  echo "<h1>Final cost is,.$final_cost;</h1>\n";
-  echo "<p>My Variable is = " . $baseprice . "</p>\n";
+  echo "<h1> Topping Cost is: $topping_costs</h1>\n";
+  echo "<h1>Subtotal is: $subtotal</h1>\n";
+  echo "<h1>Tax is: $tax</h1>\n";
+  echo "<h1>Final cost is:$final_cost</h1>\n";
+  echo "<p>My Variable is =  " . $baseprice . "</p>\n";
   ?>
 
 </body>
