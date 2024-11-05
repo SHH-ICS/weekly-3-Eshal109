5897833bff535cb5d4df812f23e6cdc00f023f09
Eshal109 <eshal.kashif@stu.ocsb.ca>
2024-11-05 15:35:40 +0000

    Assignment 3

--- handler.php
+++ handler.php
@@ -9,34 +9,53 @@
 </head>
 
 <body>
+  <div id="sample3"></div>
 
-  <?php
-  $size = "";
-  if (isset($_POST['size'])) {
-    $myVariable = $_POST['size'];
-  } else;
-  $number_toppings = -1;
-  if (isset($_POST['number_toppings'])) {
-    $myVariable = $_POST['number_toppings'];
-  } else;
-  $baseprice = [
-    'large' => 6.00,
-    'extra_large' => 10.00
-  ];
-  $topping_cost = [1, 1.75, 2.50, 3.35];
-  $hst = 0.13;
-  $topping_costs = $toppings_cost[$number_toppings - 1];
-  $subtotal = $baseprice[$size] + $topping_costs;
-  $tax = $subtotal * $hst;
-  $final_cost = $subtotal + $tax;
-  $baseprice = 0;
-  echo "<h1> Topping Cost is: $topping_costs</h1>\n";
-  echo "<h1>Subtotal is: $subtotal</h1>\n";
-  echo "<h1>Tax is: $tax</h1>\n";
-  echo "<h1>Final cost is:$final_cost</h1>\n";
-  echo "<p>My Variable is =  " . $baseprice . "</p>\n";
-  ?>
+  <!-- Always shows a header, even in smaller screens. -->
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <!-- Circumference and area -->
+        <span class="mdl-layout-title">Costs</span>
+        <!-- Add spacer, to align navigation to the right -->
+        <div class="mdl-layout-spacer"></div>
+        <!-- Navigation. We hide it in small screens. -->
+        <nav class="mdl-navigation mdl-layout--large-screen-only">
+        </nav>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="page-content">
 
-</body>
+        <body>
+          <?php
+          $size = "";
+          if (isset($_POST['size'])) {
+            $size = trim(strtolower($_POST['size']));
+          }
+
+          $number_toppings = -1;
+          if (isset($_POST['number_toppings'])) {
+            $number_toppings = ($_POST['number_toppings']);
+          }
+
+          $baseprice = [
+            'large' => 6.00,
+            'extra large' => 10.00
+          ];
+          $toppings_cost = [1, 1.75, 2.50, 3.35];
+          $hst = 0.13;
+          $toppings_costs = $toppings_cost[$number_toppings];
+          $subtotal = $baseprice[$size] + $toppings_costs;
+          $tax = $subtotal * $hst;
+          $final_cost = $subtotal + $tax;
+
+          echo "<h1>Topping cost is: " . $toppings_costs . "</h1>\n";
+          echo "<h1>Subtotal is: " . $subtotal . "</h1>\n";
+          echo "<h1>Tax is: " . $tax . "</h1>\n";
+          echo "<h1>Final cost is: " . $final_cost . "</h1>\n";
+          ?>
+
+        </body>
 
 </html>
